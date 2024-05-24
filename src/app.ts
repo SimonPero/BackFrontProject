ae23d839e1b430369b785f8883e24e3685aaaed0
Simon Pero <simondevpero@gmail.com>
2024-05-23 22:17:41 -0300

    conecte una base de datos MySQL al proyecto

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from 'express';
+import envConfig from "./config/env.config"
+import productRouter from './routes/products.router';
+import { syncDatabase } from './DAO';
+
+
+const app = express();
+const port = envConfig.port;
+
+syncDatabase();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send('Halloooo¡¡¡ America Ya :D ');
+});
+
+app.use('/api', productRouter);
+
+app.listen(port, async () => {
+    try {
+        console.log('Connection has been established successfully.');
+        console.log(`Servidor escuchando http://localhost:${port}`);
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+});
