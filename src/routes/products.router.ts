6be923f9c50266002b8df75a562ff931dd43e3b5
Simon Pero <simonraulpero@gmail.com>
2024-06-10 23:40:09 -0300

    added a delete http petition to delete prods by Id

--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import productManager from '../controllers/product.controller';
 import upload from '../utils/multer/multer';
+
 const productController = new productManager()
 const productRouter = Router();
 
@@ -8,5 +9,6 @@ productRouter.get('/products', productController.findAllProds);
 productRouter.get('/products/:id', productController.findProdById);
 productRouter.put('/products/:id', upload.single('image'), productController.updateProd);
 productRouter.post('/products', upload.single('image'), productController.addProduct);
+productRouter.delete('/products/:id', productController.deleteById);
 
 export default productRouter;
