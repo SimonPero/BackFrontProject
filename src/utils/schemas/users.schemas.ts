92b370315a75c33ea4dc575013409e67b75110d8
Simon Pero <simonraulpero@gmail.com>
2024-06-14 14:37:17 -0300

    we changed the zod schemas in order for it to work

--- src/utils/schemas/users.schemas.ts
+++ src/utils/schemas/users.schemas.ts
@@ -4,6 +4,7 @@ export const login = z.object({
     email: z.string().email(),
     password: z.string().min(5),
 });
+
 export const register = z.object({
     name: z.string().min(6),
     email: z.string().email(),
