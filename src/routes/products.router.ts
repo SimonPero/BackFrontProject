6f1587971cb944884ac79cd5a8306533c4217817
Simon Pero <simonraulpero@gmail.com>
2024-06-24 21:56:00 -0300

    we the middleware authenticateJWT in some endpoints

--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import productManager from '../controllers/product.controller';
 import upload from '../utils/multer/multer';
-
+import authenticateJWT from '../middlewares/auth';
 const productController = new productManager()
 const productRouter = Router();
 
 productRouter.get('/products', productController.findAllProds);
 productRouter.get('/products/:id', productController.findProdById);
-productRouter.put('/products/:id', upload.single('image'), productController.updateProd);
-productRouter.post('/products', upload.single('image'), productController.addProduct);
-productRouter.delete('/products/:id', productController.deleteById);
+productRouter.put('/products/:id', authenticateJWT, upload.single('image'), productController.updateProd);
+productRouter.post('/products', authenticateJWT, upload.single('image'), productController.addProduct);
+productRouter.delete('/products/:id', authenticateJWT, productController.deleteById);
 
 export default productRouter;
