3ea9cef107cff097e99924388489247c7e5430c0
Simon Pero <simonraulpero@gmail.com>
2024-06-27 02:45:19 -0300

    minor changes

--- src/utils/jwt/jwt.ts
+++ src/utils/jwt/jwt.ts
@@ -1,10 +1,10 @@
 import config from "../../config/env.config";
 import jwt from 'jsonwebtoken';
 import { UserAttributes } from "../../DAO/models/user.model";
-import { Response} from "express";
+import { Response } from "express";
 
 function signToken(user: UserAttributes) {
-    const token = jwt.sign({user}, config.jwtSecret, { expiresIn: '1d' });
+    const token = jwt.sign({ user }, config.jwtSecret, { expiresIn: '1d' });
     return token
 }
 
@@ -14,6 +14,11 @@ export function createUserToken(user: UserAttributes, res: Response, req: any) {
 
     req.session.jwt = token;
     req.session.user = user;
-    
+    res.cookie("jwt", token, {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+    });
     return res.status(201).json({ user, token });
 }
