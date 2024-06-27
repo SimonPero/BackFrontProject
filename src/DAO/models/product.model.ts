780b11ba3def5c4ec1490e5bdda146a3ca2a8c52
Simon Pero <simonraulpero@gmail.com>
2024-06-27 02:43:47 -0300

    modified the models so they actually match the database

--- src/DAO/models/product.model.ts
+++ src/DAO/models/product.model.ts
@@ -3,7 +3,7 @@ import sequelize from '../../config/database';
 
 // Define los atributos de tu modelo
 export interface ProductAttributes {
-  productID: number;
+  id: number;
   category: string;
   name: string;
   description: string;
@@ -14,11 +14,11 @@ export interface ProductAttributes {
 }
 
 // Define los atributos opcionales para la creación
-export interface ProductCreationAttributes extends Optional<ProductAttributes, 'productID'> { }
+export interface ProductCreationAttributes extends Optional<ProductAttributes, 'id'> {}
 
 // Define el modelo
 class Product extends Model<ProductAttributes, ProductCreationAttributes> implements ProductAttributes {
-  public productID!: number;
+  public id!: number;
   public category!: string;
   public name!: string;
   public description!: string;
@@ -34,7 +34,7 @@ class Product extends Model<ProductAttributes, ProductCreationAttributes> implem
 
 Product.init(
   {
-    productID: {
+    id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true,
--- src/DAO/models/user.model.ts
+++ src/DAO/models/user.model.ts
@@ -2,21 +2,21 @@ import { Model, DataTypes, Optional } from 'sequelize';
 import sequelize from '../../config/database';
 
 export interface UserAttributes {
-  customerID: number;
-  name: string;
-  surname: string;
-  email: string;
-  phone: string;
-  location: string;
-  age: number;
-  password: string;
+    id: number;
+    name: string;
+    surname: string;
+    email: string;
+    phone: string;
+    location: string;
+    age: number;
+    password: string;
 }
 
-export interface UserCreationAttributes extends Optional<UserAttributes, 'customerID'> { }
+export interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
 
 // Define el modelo
 class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
-  public customerID!: number;
+  public id!: number;
   public name!: string;
   public surname!: string;
   public email!: string;
@@ -30,45 +30,45 @@ class User extends Model<UserAttributes, UserCreationAttributes> implements User
 }
 
 User.init(
-  {
-    customerID: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      surname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      location: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      age: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      }, 
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      }
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    surname: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    location: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    phone: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    age: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
+    {
+      sequelize,
+      tableName: 'customers',
     }
-  },
-  {
-    sequelize,
-    tableName: 'customers',
-  }
-);
+  );
 
-export default User;
+export default User
