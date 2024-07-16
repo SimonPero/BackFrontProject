801215e56616b2f6c12625b4056545fe6a7bbf0c
Simon Pero <simonraulpero@gmail.com>
2024-07-16 18:48:37 -0300

    added a correctly done zod validation for addProd route

--- src/middlewares/validatorSchema.ts
+++ src/middlewares/validatorSchema.ts
@@ -4,13 +4,18 @@ import { ZodSchema } from "zod"
 type RequestProperty = "body" | "query" | "params"
 
 
-export default function validatorSchema(schema: ZodSchema, property: RequestProperty = "body"){
+export default function validatorSchema(schema: ZodSchema, property: RequestProperty = "body") {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
-            req[property] = schema.parse(req[property])
-            next()
-        } catch (error) { 
-            next(error)
+            const result = schema.safeParse(req[property]);
+            if (result.success) {
+                req[property] = result.data;
+                next();
+            } else {
+                res.status(400).json({ errors: result.error.errors });
+            }
+        } catch (error) {
+            next(error);
         }
     }
 }
