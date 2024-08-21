ac32263321bd9ee8d4c0a20ce524e65f02bad6b1
Simon Pero <simonraulpero@gmail.com>
2024-08-20 21:01:14 -0300

    complete the database configuration

---
+++ src/DAO/models/order/order.model.ts
@@ -0,0 +1,75 @@
+import { Model, DataTypes, Optional, Association, ModelStatic } from 'sequelize';
+import sequelize from '../../../config/database';
+import { User } from '../../index';
+
+// Define the attributes of your model
+export interface OrdersAttributes {
+  ordersID: number;
+  orderDate: Date;
+  customerID: number;
+  status: string;
+  shippingAddress: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+// Define the optional attributes for creation
+export interface OrdersCreationAttributes extends Optional<OrdersAttributes, 'ordersID' | 'createdAt' | 'updatedAt'> { }
+
+// Define the model
+class Order extends Model<OrdersAttributes, OrdersCreationAttributes> implements OrdersAttributes {
+  public ordersID!: number;
+  public orderDate!: Date;
+  public customerID!: number;
+  public status!: string;
+  public shippingAddress!: string;
+
+  // timestamps!
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  // Associations
+  public static associations: {
+    user: Association<Order, User>;
+  };
+
+  public static associate(models: { [key: string]: ModelStatic<Model> }) {
+    Order.belongsTo(models.User, { foreignKey: 'customerID', as: 'user' });
+  }
+}
+
+Order.init(
+  {
+    ordersID: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    orderDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    customerID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    shippingAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  },
+  {
+    sequelize,
+    tableName: 'orders',
+    timestamps: true,
+    modelName: 'Order',
+  }
+);
+
+export default Order;
---
+++ src/DAO/models/order/orderDetail.model.ts
@@ -0,0 +1,93 @@
+import {
+  Model,
+  DataTypes,
+  Optional,
+  Association,
+  ModelStatic,
+} from "sequelize";
+import sequelize from "../../../config/database";
+import { Product, Order } from "../../index";
+
+// Define the attributes of your model
+export interface OrderDetailsAttributes {
+  orderDetailID: number;
+  orderID: number;
+  productID: number;
+  quantity: number;
+  price: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+// Define the optional attributes for creation
+export interface OrdersDetailsCreationAttributes
+  extends Optional<
+    OrderDetailsAttributes,
+    "orderDetailID" | "createdAt" | "updatedAt"
+  > {}
+
+// Define the model
+class OrderDetail
+  extends Model<OrderDetailsAttributes, OrdersDetailsCreationAttributes>
+  implements OrderDetailsAttributes
+{
+  public orderDetailID!: number;
+  public orderID!: number;
+  public productID!: number;
+  public quantity!: number;
+  public price!: number;
+
+  // timestamps!
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  // Associations
+  public static associations: {
+    order: Association<OrderDetail, Order>;
+    product: Association<OrderDetail, Product>;
+  };
+
+  public static associate(models: { [key: string]: ModelStatic<Model> }) {
+    OrderDetail.belongsTo(models.Order, { foreignKey: "orderID", as: "order" });
+    OrderDetail.belongsTo(models.Product, {
+      foreignKey: "productID",
+      as: "product",
+    });
+  }
+}
+
+OrderDetail.init(
+  {
+    orderDetailID: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    orderID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    productID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  },
+  {
+    sequelize,
+    tableName: "orders",
+    timestamps: true,
+    modelName: "Order",
+  }
+);
+
+export default OrderDetail;
