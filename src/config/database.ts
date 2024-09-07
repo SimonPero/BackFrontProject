b6c27ec631e5c25a2a2f0b8fe292b810c33b4921
Simon Pero <simonraulpero@gmail.com>
2024-09-06 21:15:27 -0300

    Changed from MysqlWorkbench to SSMS

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,17 @@
-import { Sequelize } from 'sequelize';
-import envConfig from './env.config';
-const db = envConfig.database;
-const user = envConfig.mySqlUser;
-const pass = envConfig.mySqlPass;
-const sequelize = new Sequelize(db, user, pass, {
-    host: 'localhost',
-    dialect: 'mysql',
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize("Ecommerce", "Simon", "1234", {
+  dialect: "mssql",
+  host: "127.0.0.1",
+  port:1433,
+  dialectOptions: {
+    options: {
+      encrypt: false,
+      trustServerCertificate: true,
+      useUTC: false,
+      dateFirst: 1,
+    },
+  },
 });
 
 export default sequelize;
--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,22 +1,21 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 // Determina el entorno (por defecto 'development' si no está configurado)
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 
 // Carga el archivo .env correspondiente al entorno
 dotenv.config({ path: `.env.${env}` });
 
 // Exporta las variables de entorno, asegurándose de que no sean undefined
 const config = {
-  port: process.env.PORT || '8080',
-  database: process.env.DATABASE || '',
-  mySqlUser: process.env.MYSQLUSER || '',
-  mySqlPass: process.env.MYSQLPASS || '',
-  jwtSecret:process.env.JWT_SECRET || ''
+  port: process.env.PORT || "8080",
+  databaseUrl: process.env.DATABASE_URL || "",
+  jwtSecret: process.env.JWT_SECRET || "",
+  encrypt: process.env.ENCRYPT || "",
 };
 
-if (!config.database || !config.mySqlUser || !config.mySqlPass) {
-  throw new Error('Missing environment variables for database connection');
+if (!config.databaseUrl) {
+  throw new Error("Missing environment variables for database connection");
 }
 
 export default config;
